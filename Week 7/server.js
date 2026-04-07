eea38620f10a45ea05bd95839dee00136512f341
cyber-goblinbug <wjw67@miami.edu>
2026-04-07 17:40:43 -0400

    fredf

--- Week 7/server.js
+++ Week 7/server.js
@@ -100,7 +100,15 @@ app.use(express.static(path.join(__dirname, "static")));
 // Parse the body of incoming requests with urlencoded payloads and is based on body-parser. This middleware is used to parse the body of incoming requests and make it available under the req.body property. The extended: true option allows for rich objects and arrays to be encoded into the URL-encoded format, which can be useful for complex data structures.
 app.use(express.urlencoded({ extended: true }));
 // data
-
+// Set up basic CORS headers for communicating  with APIs
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 // generate routes
 app.get("/", async (req, res) => {
   // Homepage route
@@ -205,6 +213,12 @@ app.post("/images", async (req, res) => {
   });
   await newImage.save();
   res.send("Image added successfully");
+});
+
+// setup bvasic api routes
+app.get("/api/destinations", async (req, res) => {
+  const destinations = await Destination.find().lean();
+  res.json(destinations);
 });
 
 // start the server
